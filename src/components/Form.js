0a3dbe1267b1d8c0ef14012a466de9ff9bec0f97
charlie763 <cwisoff@gmail.com>
2020-09-23 09:01:23 -0600

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,10 +6,17 @@ class Form extends React.Component {
     lastName: "Henry"
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
-    })
+    }, () => console.log(this.state))
   }
 
   handleLastNameChange = event => {
@@ -20,7 +27,7 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form>
+      <form onSubmit={e => this.handleSubmit(e)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
